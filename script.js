99d37ff2ca2371c54ae49bd66b0d6452741a6b9c
Nicolas <nicoa.castro@duocuc.cl>
2025-10-18 22:34:01 -0300

    Añadir campo 'email' a la data de usuarios (RF2) dev1

---
+++ script.js
@@ -0,0 +1,5 @@
+const users = [
+    { id: 1, name: "Alice", email: "alice@example.com", product: "DataViz" },
+    { id: 2, name: "Bob", email: "bob@example.com", product: "TechSolutions" },
+    { id: 3, name: "Charlie", email: "charlie@example.com", product: "DataViz" }
+];
