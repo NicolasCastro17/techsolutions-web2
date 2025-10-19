3526d91c26e0f234aefc392673350ac4b2a82cf8
Nicolas <nicoa.castro@duocuc.cl>
2025-10-18 22:37:52 -0300

    Añadir campo 'age' a la data de usuarios (RF3) dev2

---
+++ script.js
@@ -0,0 +1,5 @@
+const users = [
+    { id: 1, name: "Alice", age: 30, product: "DataViz" },
+    { id: 2, name: "Bob", age: 25, product: "TechSolutions" },
+    { id: 3, name: "Charlie", age: 40, product: "DataViz" }
+];
